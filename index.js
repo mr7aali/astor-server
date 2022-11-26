c24c666b7a0be01d49727ec7b9d6ee6f379f9604
mr7aali <mr7aali@gmail.com>
2022-11-26 20:46:06 +0600

    added env

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//pass      Thzl1NGlAvdi3pww
+//user    astor
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lopokh6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/',async(req,res)=>{
     res.send('PuranMobile Is running');
 })
