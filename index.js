15a96e37438d4c01462a0b9d1b20e5587af223fb
mr7aali <mr7aali@gmail.com>
2022-11-27 00:02:38 +0600

    gain nedded catagory from all catagory

--- index.js
+++ index.js
@@ -19,8 +19,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run(){
+    try{
+        const brandCollection = client.db('Astor').collection('brand');
+        const phoneCollection =client.db('Astor').collection('catagories');
+
+        app.get('/brand',async(req,res)=>{
+            const query ={};
+           const options = await brandCollection.find(query).toArray();
+           res.send(options);
+        })
+        
+        app.get('/categorys/:id', async(req,res)=>{
+
+            const brandID = req.params.id;
+            console.log(brandID);
+
+            const query ={_id:ObjectId(brandID)};
+            const singleBrand =await brandCollection.find(query).toArray();
+            const NeddedBrand= singleBrand[0].brandName;
+            console.log("from clind ", NeddedBrand)
+                    
+            const query2={brandName:NeddedBrand};
+            const phones= await phoneCollection.find(query2).toArray();
+            console.log(phones);
+            
+          
 
 
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.error(err));
 
 
 
